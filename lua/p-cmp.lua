a9473baa5fc8d5a7dcd6b8208e8bac155810daf1
DYYYYYYYF <64484752+DYYYYYYYF@users.noreply.github.com>
2023-06-22 15:49:17 +0800

    User 'Tab' to next params

--- lua/p-cmp.lua
+++ lua/p-cmp.lua
@@ -25,20 +25,26 @@ cmp.setup({
     ['<C-c>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" })
-  }),
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+        -- elseif has_words_before() then
+        --   cmp.complete()
+        else
+          fallback()
+        end
+      end,{ "i", "s" }),
+     ['<S-Tab>'] = cmp.mapping(function(core, fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+        else
+          fallback()
+        end
+      end,{ "i", "s" }),
+   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp',
       entry_filter = function(entry)
@@ -89,6 +95,15 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['clangd'].setup {
+    capabilities = capabilities
+}
+
 
 vim.cmd([[
 set completeopt=menuone,noinsert,noselect
