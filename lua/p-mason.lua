b2cf75c07cae8e890c29c3fcaee4e4b4ee2053bf
UncleDon <64484752+DYYYYYYYF@users.noreply.github.com>
2024-04-19 18:02:26 +0800

    Ignore unused context

--- lua/p-mason.lua
+++ lua/p-mason.lua
@@ -2,9 +2,6 @@ require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
     'sumneko_lua',
-    'tsserver',
-    'graphql',
-    'rust_analyzer'
   },
   automatic_installation = true,
 })
--- lua/p-treesitter.lua
+++ lua/p-treesitter.lua
@@ -1,9 +1,8 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "tsx", "lua", "rust", "json", "graphql", "regex", "vim", "cpp" },
+    ensure_installed = { "tsx", "lua", "json", "vim", "cpp" },
 
     sync_install = false,
     auto_install = true,
-    ignore_install = { "javascript" },
 
     highlight = {
         enable = true,
@@ -25,12 +24,10 @@ require 'nvim-treesitter.configs'.setup {
 }
 
 require('template-string').setup({
-    filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'python', 'cpp' }, -- filetypes where the plugin is active
+    filetypes = { 'python', 'cpp' }, -- filetypes where the plugin is active
     jsx_brackets = true, -- must add brackets to jsx attributes
     remove_template_string = false, -- remove backticks when there are no template string
     restore_quotes = {
         -- quotes used when "remove_template_string" option is enabled
-        normal = [[']],
-        jsx = [["]],
     },
 })
