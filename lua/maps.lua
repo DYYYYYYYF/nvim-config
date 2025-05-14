52e12db22ddd4f7aa1f452c0818631d03b9a3cc0
gituserdon <dongyf>
2025-05-14 11:09:13 +0800

    add ignore patterns

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,19 +1,21 @@
 vim.g.mapleader = "'"
 local keymap = vim.keymap
 
+-- normal mode
 keymap.set('n', '<c-a>', 'gg<S-v>G')
-
 keymap.set('n', '<leader>vs', ':vsplit<Return><C-w>w', { silent = true })
 keymap.set('n', 'f', '<C-w>w')
-
 keymap.set('n', 'H', '^')
 keymap.set('n', 'L', '$')
 
-local opts = { noremap = true, silent = true }
+-- terminal mode
+keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
+local opts = { noremap = true, silent = true }
 -- cmake source mapping
 keymap.set('n', "<leader><leader>s", ":!cmake -S . -B build<CR>", {noremap = true})
 -- cmake build mapping
 keymap.set('n', "<leader><leader>b", ":!cmake --build build -j4<CR>", {noremap = true})
 -- cmake test mapping
 keymap.set('n', "<leader><leader>t", ":!cmake --build build -j2 --target test<CR>", {noremap = true})
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,13 +19,29 @@ return {
         local telescope = require("telescope")
         telescope.setup({
           defaults = {
+            preview = true,      -- 或者 preview = false  -- 关闭预览加快检索速度
+            prompt_prefix = "🔍 ",
+            selection_caret = "➤ ",
+            sorting_strategy = "ascending",
             layout_config = {
               horizontal = { width = 0.9 },
               preview_cutoff = 120,
             },
-            prompt_prefix = "🔍 ",
-            selection_caret = "➤ ",
-            sorting_strategy = "ascending",
+            -- 忽略哪些文件及目录，可以加快检索速度
+            file_ignore_patterns = {
+              ".git",      -- 忽略 .git 目录
+              ".svn",      -- 忽略 .svn 目录
+              ".idea",     -- 忽略 JetBrains 项目文件
+              "node_modules",
+              "build",
+              "bin",
+              "obj",
+              "out",
+              ".vs",       -- 忽略 Visual Studio 项目目录
+              ".xcworkspace", -- Xcode workspace
+              ".xcodeproj",   -- Xcode project
+              ".DS_Store",     -- macOS 文件
+            },
           },
           pickers = {
             find_files = {
@@ -33,7 +49,7 @@ return {
             },
           },
         })
-  
+
         -- 加载 FZF 加速插件（如果有）
         pcall(telescope.load_extension, "fzf")
       end,
